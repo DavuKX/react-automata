1f0a430bffcf2d7dc655d1d2b2c80d55f06246f4
Valentina9990 <dania.v.sarmiento@gmail.com>
2023-10-03 20:58:11 -0500

    Structure for changing languages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,26 @@ import NavBar from "@/components/navBar/navBar";
 import ToolsSection from "@/components/toolsSection/toolsSection";
 import Grid from '@mui/material/Unstable_Grid2'
 
+import global_en from '@/locales/en/global.json';
+import global_es from '@/locales/es/global.json';
+import global_pt from '@/locales/pt/global.json';
+import i18next from 'i18next';
+
+i18next.init({
+  interpolation: { escapeValue: false },  // React already does escaping
+  lng: 'en',                              // language to use
+  resources: {
+    en: {
+      global: global_en           
+    },
+    es: {
+      global: global_es
+    },
+    pt: {
+      global: global_pt
+    },
+  },
+});
 
 export default function Home() {
     return (
