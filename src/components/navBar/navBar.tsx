0ce49179fba426efb1be46653b403fe0a2ece354
DavuKX <103606165+DavuKX@users.noreply.github.com>
2023-10-03 19:52:28 -0500

    made toolsSection ui

---
+++ src/components/navBar/navBar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const NavBar = ({}) => {
+
+    return (
+        <div className="mx-3 py-5 text-center">
+            <div className="flex gap-3 flex-wrap justify-around">
+                <div className="bg-clip-text font-bold text-2xl">
+                    Automata solver
+                </div>
+            </div>
+        </div>
+    );
+};
+
+NavBar.propTypes = {
+
+};
+
+export default NavBar;
---
+++ src/components/toolsSection/toolsSection.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Box, Button, Paper, Slider, TextField, Typography} from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+
+const ToolsSection = ({}) => {
+    return (
+        <Paper elevation={4}>
+            <div className="p-6">
+                <Grid>
+                    <Grid xs={12}>
+                        <TextField
+                            id="outlined-multiline-static"
+                            label="Ingrese las palabras"
+                            multiline
+                            defaultValue=""
+                            fullWidth
+                            minRows={4}
+                            maxRows={10}
+                        />
+                    </Grid>
+                    <Grid>
+                        <Box>
+                            <Typography>Automata speed</Typography>
+                            <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" min={10}/>
+                        </Box>
+                    </Grid>
+                    <Grid xs={12}>
+                        <Button variant="outlined" fullWidth>Run Automata</Button>
+                    </Grid>
+                </Grid>
+            </div>
+        </Paper>
+    );
+};
+
+ToolsSection.propTypes = {
+
+};
+
+export default ToolsSection;
