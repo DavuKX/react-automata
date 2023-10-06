cc38142fb8cfc4a8cc0c32a64528e381eee60a0b
anuarthr <anuarthrincongonzalez@gmail.com>
2023-10-06 15:08:49 -0500

    Automaton spoken

--- src/businessLogic/validateSection.tsx
+++ src/businessLogic/validateSection.tsx
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Paper } from '@mui/material';
 import { validateString } from './validateString';
 
 export const ValidateSection = ({ inputString }) => {
   const [validationResult, setValidationResult] = useState('');
+  const [spokenMessage, setSpokenMessage] = useState('');
+
+  useEffect(() => {
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(spokenMessage);
+    synth.speak(utterance);
+  }, [spokenMessage]);
 
   const handleValidate = () => {
     if (inputString && inputString.length > 0) {
       const isValid = validateString(inputString);
-      setValidationResult(isValid ? 'accepted' : 'rejected');
+      const message = isValid ? 'String accepted' : 'String rejected';
+      setValidationResult(message);
+      setSpokenMessage(message);
     } else {
       setValidationResult('Please enter a valid input string.');
+      setSpokenMessage('Please enter a valid input string.');
     }
   };      
 
