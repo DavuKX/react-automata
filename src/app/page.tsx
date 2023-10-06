b20fe980a9dc7939f11b8b19b5006e1baad6f24d
anuarthr <anuarthrincongonzalez@gmail.com>
2023-10-06 14:50:22 -0500

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,22 @@ import NavBar from "@/components/navBar/navBar";
 import ToolsSection from "@/components/toolsSection/toolsSection";
 import Grid from '@mui/material/Unstable_Grid2'
 import AutomatonGraph from "@/components/automatonGraph/automatonGraph";
-import { useTranslation} from 'react-i18next';
 import React, { useState } from 'react';
 import ValidateSection from "@/businessLogic/validateSection";
 import { validateString } from "@/businessLogic/validateString";
-import '@/i18n'
 
 export default function Home() {
-    const {t, i18n} = useTranslation();
-    
-    const changeLanguage = (language) => {
-        i18n.changeLanguage(language);
-    } 
+    const [validationResult, setValidationResult] = useState('');
+    const [inputWords, setInputWords] = useState('');
+
+    const handleValidate = () => {
+        const isValid = validateString(inputWords);
+        setValidationResult(isValid ? 'String accepted' : 'String rejected');
+    };
+
+    const handleWordsChange = (words) => {
+        setInputWords(words);
+    };
 
     return (
         <div className='h-screen w-full bg-white'>
@@ -31,7 +35,6 @@ export default function Home() {
                 </Grid>
                 <Grid xs={12}></Grid>
             </Grid>
-            <div>{t("title")}</div>
         </div>
     );
 }
