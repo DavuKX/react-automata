052031e7717364f2751293dc83ec83c4e0fc1360
Valentina9990 <dania.v.sarmiento@gmail.com>
2023-10-07 19:18:09 -0500

    Graph fixes

--- src/components/automatonGraph/automatonGraph.tsx
+++ src/components/automatonGraph/automatonGraph.tsx
@@ -1,82 +1,172 @@
 "use client"
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import PropTypes from 'prop-types';
-import {Graph} from "react-d3-graph";
+
 import {Paper} from "@mui/material";
+import CytoscapeComponent from "react-cytoscapejs";
 
-class AutomatonGraph extends Component {
-    render() {
-        const myConfig = {
-            nodeHighlightBehavior: true,
-            node: {
-                color: 'purple',
-                size: 120,
-                highlightStrokeColor: 'blue'
-            },
-            link: {
-                highlightColor: 'lightblue'
-            },
-            staticGraph: false,
-            d3: {
-                alphaTarget: 0.05,
-                gravity: -150,
-                linkLength: 120,
-                linkStrength: 2,
-            },
-        };
+export default function AAutomatonGraph() {
+  const [width, setWith] = useState("100%");
+  const [height, setHeight] = useState("400px");
+  const [graphData, setGraphData] = useState({
+    nodes: [
+      { data: { id: "0", label: "q0"} },
+      { data: { id: "1", label: "q1"} },
+      { data: { id: "2", label: "q2"} },
+      { data: { id: "3", label: "q3"} },
+      { data: { id: "4", label: "q4"} },
+      { data: { id: "5", label: "q5"} },
+      { data: { id: "6", label: "q6"} },
+      { data: { id: "7", label: "q7"} },
+      { data: { id: "8", label: "q8"} },
+      { data: { id: "9", label: "q9"} },
+      { data: { id: "10", label: "q10"} },
+      { data: { id: "11", label: "q11"} },
+      { data: { id: "12", label: "q12"} },
+      { data: { id: "13", label: "q13"} },
+      { data: { id: "14", label: "q14"} }
+    ],
+    edges: [
+      {
+        data: { source: "0", target: "1", label: "B" }
+      },
+      {
+        data: { source: "0", target: "2", label: "A" }
+      },
+      {
+        data: { source: "2", target: "3", label: "A" }
+      },
+      {
+        data: { source: "2", target: "4", label: "B" }
+      },
+      {
+        data: { source: "3", target: "5", label: "A" }
+      },
+      {
+        data: { source: "4", target: "5", label: "A" }
+      },
+      {
+        data: { source: "3", target: "14", label: "B" }
+      },
+      {
+        data: { source: "4", target: "14", label: "B" }
+      },
+      {
+        data: { source: "5", target: "7", label: "B" }
+      },
+      {
+        data: { source: "7", target: "14", label: "B" }
+      },
+      {
+        data: { source: "5", target: "6", label: "A" }
+      },
+      {
+        data: { source: "7", target: "8", label: "A" }
+      },
+      {
+        data: { source: "8", target: "9", label: "A" }
+      },
+      {
+        data: { source: "9", target: "14", label: "B" }
+      },
+      {
+        data: { source: "6", target: "14", label: "B" }
+      },
+      {
+        data: { source: "6", target: "7", label: "A" }
+      },
+      {
+        data: { source: "8", target: "10", label: "B" }
+      },
+      {
+        data: { source: "10", target: "14", label: "B" }
+      },
+      {
+        data: { source: "10", target: "11", label: "A" }
+      },
+      {
+        data: { source: "9", target: "11", label: "A" }
+      },
+      {
+        data: { source: "11", target: "12", label: "A" }
+      },
+      {
+        data: { source: "11", target: "13", label: "B" }
+      }
+    ]
+  });
 
-        const data = {
-            nodes: [
-                {id: "start"},
-                {id: "s0"},
-                {id: "s1"},
-                {id: "s2"},
-                {id: "s3"},
-                {id: "s4"},
-                {id: "s5"},
-                {id: "s6"},
-                {id: "s7"},
-                {id: "s8"},
-                {id: "s9"},
-                {id: "s10"},
-                {id: "s11"},
-                {id: "s12"},
-                {id: "s13"},
-            ],
-            links: [
-                {source: "start", target: "s0"},
-                {source: "start", target: "s1"},
-                {source: "s1", target: "s2"},
-                {source: "s1", target: "s3"},
-                {source: "s2", target: "s4"},
-                {source: "s2", target: "s13"},
-                {source: "s3", target: "s4"},
-                {source: "s3", target: "s13"},
-                {source: "s4", target: "s5"},
-                {source: "s4", target: "s6"},
-                {source: "s5", target: "s7"},
-                {source: "s6", target: "s13"},
-                {source: "s7", target: "s8"},
-                {source: "s7", target: "s9"},
-                {source: "s8", target: "s10"},
-                {source: "s8", target: "s13"},
-                {source: "s9", target: "s13"},
-                {source: "s9", target: "s10"},
-                {source: "s10", target: "s12"},
-                {source: "s10", target: "s11"},
-            ]
-        }
+  const layout = {
+    name: "breadthfirst",
+    fit: true,
+    // circle: true,
+    directed: true,
+    padding: 50,
+    // spacingFactor: 1.5,
+    animate: true,
+    animationDuration: 1000,
+    avoidOverlap: true,
+    nodeDimensionsIncludeLabels: false
+  };
 
-        return (
-            <Paper elevation={4}>
-                <Graph
-                    id='id'
-                    data={data}
-                    config={myConfig}
-                />
-            </Paper>
-        );
+  const styleSheet = [
+  {
+    selector: "node[label]",
+    style: {
+      label: "data(label)",
+      "text-halign": "center",
+      "text-valign": "center",
     }
-}
+  },
+  {
+    selector: "edge[label]",
+    style: {
+      label: "data(label)",
+      "curve-style": "bezier",
+      "target-arrow-shape": "triangle",
+    }
+  }
+];
 
-export default AutomatonGraph;
+  let myCyRef;
+
+  return (
+    <>
+      <div>
+        <h1>Cytoscape example</h1>
+        <div
+          style={{
+            border: "1px solid",
+            backgroundColor: "#f5f6fe"
+          }}
+        >
+          <CytoscapeComponent
+            elements={CytoscapeComponent.normalizeElements(graphData)}
+            // pan={{ x: 200, y: 200 }}
+            style={{ width: width, height: height }}
+            zoomingEnabled={true}
+            maxZoom={3}
+            minZoom={0.1}
+            autounselectify={false}
+            boxSelectionEnabled={true}
+            layout={layout}
+            stylesheet={styleSheet}
+            cy={cy => {
+              myCyRef = cy;
+
+              console.log("EVT", cy);
+
+              cy.on("tap", "node", evt => {
+                var node = evt.target;
+                console.log("EVT", evt);
+                console.log("TARGET", node.data());
+                console.log("TARGET TYPE", typeof node[0]);
+              });
+            }}
+            abc={console.log("myCyRef", myCyRef)}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
