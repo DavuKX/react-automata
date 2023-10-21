8123ea229e2e1c8f2911e2479831c96fa55a041d
DavuKX <103606165+DavuKX@users.noreply.github.com>
2023-10-21 18:15:51 -0500

    make application no ssr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,12 @@
 "use client"
-import NavBar from "@/components/navBar/navBar";
-import ToolsSection from "@/components/toolsSection/toolsSection";
-import Grid from '@mui/material/Unstable_Grid2'
-import AutomatonGraph from "@/components/automatonGraph/automatonGraph";
-import React, {useState} from 'react';
-import {graphData} from "@/constans";
-import {useValidationHistory} from "@/businessLogic/validationHistory";
-import {ValidationHistoryComponent} from "@/components/validationHistoryComponent/validationHistoryComponent";
-
+import React from 'react';
+import dynamic from "next/dynamic";
+const NoSSR = dynamic(() => import("@/components/home/home"), {ssr: false});
 export default function Home() {
-    const [inputWords, setInputWords] = useState('');
-    const {validationHistory, addValidationToHistory} = useValidationHistory();
-    const [currentNodes, setCurrentNodes] = useState({
-        currentState: "",
-        newState: "",
-    })
-
-    const handleWordsChange = (words: string) => {
-        setInputWords(words);
-    };
 
     return (
-        <div className='w-full bg-white'>
-            <NavBar/>
-            <Grid container spacing={2} style={{margin: '0 20px'}}>
-                <Grid xs={12}>
-                    <AutomatonGraph graphData={graphData} state={currentNodes}/>
-                </Grid>
-                <Grid lg={6} md={12} xs={12}>
-                    <ToolsSection
-                        onWordsChanged={handleWordsChange}
-                        inputWords={inputWords}
-                        onFinishedValidation={addValidationToHistory}
-                        onStateChanged={(currentState, newState) => setCurrentNodes({
-                            currentState: currentState,
-                            newState: newState
-                        })}
-                    />
-                </Grid>
-                <Grid lg={6} md={12} xs={12}>
-                    <ValidationHistoryComponent history={validationHistory}/>
-                </Grid>
-            </Grid>
-        </div>
+        <>
+            <NoSSR/>
+        </>
     );
 }
---
+++ src/components/home/home.tsx
@@ -0,0 +1,46 @@
+import React, {FC, useState} from 'react';
+import {useValidationHistory} from "@/businessLogic/validationHistory";
+import NavBar from "@/components/navBar/navBar";
+import Grid from "@mui/material/Unstable_Grid2";
+import AutomatonGraph from "@/components/automatonGraph/automatonGraph";
+import {graphData} from "@/constans";
+import ToolsSection from "@/components/toolsSection/toolsSection";
+import {ValidationHistoryComponent} from "@/components/validationHistoryComponent/validationHistoryComponent";
+
+export default function Home(): JSX.Element {
+    const [inputWords, setInputWords] = useState('');
+    const {validationHistory, addValidationToHistory} = useValidationHistory();
+    const [currentNodes, setCurrentNodes] = useState({
+        currentState: "",
+        newState: "",
+    })
+
+    const handleWordsChange = (words: string): void => {
+        setInputWords(words);
+    };
+
+    return (
+        <div className='w-full bg-white'>
+            <NavBar/>
+            <Grid container spacing={2} style={{margin: '0 20px'}}>
+                <Grid xs={12}>
+                    <AutomatonGraph graphData={graphData} state={currentNodes}/>
+                </Grid>
+                <Grid lg={6} md={12} xs={12}>
+                    <ToolsSection
+                        onWordsChanged={handleWordsChange}
+                        inputWords={inputWords}
+                        onFinishedValidation={addValidationToHistory}
+                        onStateChanged={(currentState, newState) => setCurrentNodes({
+                            currentState: currentState,
+                            newState: newState
+                        })}
+                    />
+                </Grid>
+                <Grid lg={6} md={12} xs={12}>
+                    <ValidationHistoryComponent history={validationHistory}/>
+                </Grid>
+            </Grid>
+        </div>
+    );
+};
