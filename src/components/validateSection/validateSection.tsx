980ec39b35c55f843397cc504500188f2aa4fe1b
DavuKX <103606165+DavuKX@users.noreply.github.com>
2023-11-08 19:22:15 -0500

    refactor validate

--- src/components/validateSection/validateSection.tsx
+++ src/components/validateSection/validateSection.tsx
@@ -1,44 +1,19 @@
-import React, {useContext, useState} from 'react';
+import React, {useState} from 'react';
 import {Button} from '@mui/material';
 import {useTranslation} from 'react-i18next';
 import '@/i18n';
-import {AutomatonTypeContext} from "@/Contexts/automatonTypeContext";
-import {finiteAutomaton, pushdownAutomaton} from "@/constans";
 import {validationResultType} from "@/types/validationResultType";
-import { useCookies } from 'next-client-cookies';
+import { useValidationLogic } from './validationLogic';
 
 interface ValidateSectionProps {
     inputString: string;
     onFinishedValidation: (result: validationResultType) => void;
 }
 
-const automatonGraphData ={
-    finite: finiteAutomaton,
-    pushdown: pushdownAutomaton,
-}
-
 const ValidateSection: React.FC<ValidateSectionProps> = ({inputString, onFinishedValidation}) => {
     const {t} = useTranslation();
     const [validInputMessageVisible, setValidInputMessageVisible] = useState(false);
-    const automatonType = useContext(AutomatonTypeContext);
-    const cookies = useCookies();
-
-    const validateWord = async (inputString: string): Promise<validationResultType> => {
-        return await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + '/api/validate-automaton', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                automaton_data: automatonGraphData[automatonType.state],
-                automaton_type: automatonType.state,
-                word: inputString,
-                path: [],
-                uuid: cookies.get('uuid')
-            })
-
-        }).then((res) => res.json() as Promise<validationResultType>);
-    };
+    const { validateWord } = useValidationLogic();
 
     const handleValidate = async () => {
         if (inputString && inputString.length > 0) {
---
+++ src/components/validateSection/validationLogic.tsx
@@ -0,0 +1,34 @@
+import {useContext} from 'react';
+import {AutomatonTypeContext} from "@/contexts/automatonTypeContext";
+import {finiteAutomaton, pushdownAutomaton} from "@/constans";
+import {validationResultType} from "@/types/validationResultType";
+import { useCookies } from 'next-client-cookies';
+
+const automatonGraphData ={
+    finite: finiteAutomaton,
+    pushdown: pushdownAutomaton,
+}
+
+export const useValidationLogic = () => {
+    const automatonType = useContext(AutomatonTypeContext);
+    const cookies = useCookies();
+
+    const validateWord = async (inputString: string): Promise<validationResultType> => {
+        return await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + '/api/validate-automaton', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                automaton_data: automatonGraphData[automatonType.state],
+                automaton_type: automatonType.state,
+                word: inputString,
+                path: [],
+                uuid: cookies.get('uuid')
+            })
+
+        }).then((res) => res.json() as Promise<validationResultType>);
+    };
+
+    return { validateWord };
+};
